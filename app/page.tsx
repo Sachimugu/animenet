ee53bf35de65150a68091fac298c34f6c7649aac
sachimugu <sachiimugu@gmail.com>
2023-12-24 16:04:36 +0100

    key prop

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ async function Home() {
 
         <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
           {data.map((item: AnimeProp, index: any) => (
-            <Link href={`details/${item.id}`}>
+            <Link href={`details/${item.id}`} key={item.id}>
               <AnimeCard key={item.id} anime={item} index={index} />
             </Link>
           ))}
